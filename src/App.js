ee56054c13c9697031dd5da8655653bdcedb721f
levaniakobidze <90929516+levaniakobidze@users.noreply.github.com>
2022-02-24 00:11:15 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import {AiFillGithub} from 'react-icons/ai'
+
+const url =
+  "https://api.github.com/users?fbclid=IwAR38FgsXaRvSOfHASafpU5-yGjCUK9lDNQiBn4TJlWpvxP_Fry48lg6a8-o";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [searchTerm, setSerachTerm] = useState("");
+
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSerachTerm(e.target.value);
+  };
+
+  console.log(users.length);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="whole-wrapper">
+      <Navbar />
+      <div className="main-wrapper">
+        <div className="input-cont">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="type here"
+            onChange={handleChange}
+            onSubmit={(e) => e.preventDefault()}
+          />
+        </div>
+        <div className="underline"></div>
+        <div className="cards-wrapper">
+          {users
+            .filter((val) => {
+              if (searchTerm === "") {
+                return val;
+              } else if (
+                val.login.toLowerCase().includes(searchTerm.toLowerCase())
+              ) {
+                return val;
+              }
+            })
+            .map((user) => {
+              return (
+                <>
+                  <ul key={user.id} className="cards-cont">
+                   
+                    <div className="card-cont">
+                      <a  className="link" href={user.html_url} target='_blank'  >
+                      <div className="image">
+                        <img src={user.avatar_url} alt="das" />
+                      </div>
+                      <div className="card-info">
+                        <h1>{user.login} <AiFillGithub /></h1>
+                      </div>
+                      </a>
+                    </div>
+                
+                  </ul>
+                </>
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Navbar.css'
+import {AiFillGithub} from 'react-icons/ai'
+import {AiOutlineHome} from 'react-icons/ai'
+
+function Navbar() {
+  return (
+  <div className="navbar">
+
+      <div className="navbar-content">
+     <div className="logo-cont">
+        <AiFillGithub  className='logo'/>
+        <h1>TyPeHeAd</h1>
+     </div>
+     <ul className='navigation'>
+         <li>
+             <AiOutlineHome className='home-logo' />
+             HOME</li>
+     </ul>
+     </div>
+
+
+  </div>
+  )
+}
+
+export default Navbar
