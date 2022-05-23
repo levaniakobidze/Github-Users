3da4dd3f05e60fbdade1ed27dc443d0a706f570e
levaniakobidze <levaniakobidze25@gmail.com>
2022-05-23 17:30:00 +0400

    style changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,25 @@
-import React from 'react'
-import './Navbar.css'
-import {AiFillGithub} from 'react-icons/ai'
-import {AiOutlineHome} from 'react-icons/ai'
+import React from "react";
+import "./Navbar.css";
+import { AiFillGithub } from "react-icons/ai";
+import { AiOutlineHome } from "react-icons/ai";
 
 function Navbar() {
   return (
-  <div className="navbar">
-
-      <div className="navbar-content">
-     <div className="logo-cont">
-        <AiFillGithub  className='logo'/>
-        <h1>TyPeHeAd</h1>
-     </div>
-     <ul className='navigation'>
-         <li>
-             <AiOutlineHome className='home-logo' />
-             HOME</li>
-     </ul>
-     </div>
-
-
-  </div>
-  )
+    <div className='navbar'>
+      <div className='navbar-content'>
+        <div className='logo-cont'>
+          <AiFillGithub className='logo' />
+          <h1>Search</h1>
+        </div>
+        <ul className='navigation'>
+          <li>
+            <AiOutlineHome className='home-logo' />
+            HOME
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
